519693f7e64bf01457f106b719847f2037e78dc8
rif <rif@rjas.ro>
2021-04-10 13:12:40 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package acton
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+      <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'acton' ); ?></a>
+
+      <header id="masthead" class="site-header">
+        <div class="site-branding">
+          <section class="hero is-fullheight is-info">
+            <div class="hero-head">
+              <nav class="navbar">
+                <div class="container is-fluid">
+                  <div class="navbar-brand">
+                    <a class="navbar-item">
+                      <?php the_custom_logo();?>
+                    </a>
+                  </div>
+                  <div id="navbarMenuHeroA" class="navbar-menu">
+                    <div class="navbar-end">
+                      <nav id="site-navigation" class="main-navigation">
+                        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'acton' ); ?></button>
+                        <?php
+                        wp_nav_menu(
+                          array(
+                            'theme_location' => 'menu-1',
+                            'menu_id'        => 'primary-menu',
+                          )
+                        );
+                        ?>
+                      </nav><!-- #site-navigation -->
+                    </div><!-- .navbar-end -->
+                  </div><!-- #navbarMenuHeroA -->
+                </div><!-- .container -->
+              </nav><!-- .navbar -->
+            </div><!-- .hero-head -->
+            <!-- Hero content: will be in the middle -->
+            <div class="hero-body">
+              <div class="container has-text-centered">
+                <?php
+                if ( is_front_page() && is_home() ) :
+                ?>
+                  <h1 class="title has-text-weight-bold is-size-1 site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php
+                else :
+                ?>
+                  <p class="title has-text-weight-bold is-size-1 site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
+                endif;
+                $acton_description = get_bloginfo( 'description', 'display' );
+                if ( $acton_description || is_customize_preview() ) :
+                ?>
+                  <p class="subtitle is-size-2 site-description"><?php echo $acton_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                <?php endif; ?>
+                <a href="#" id="learn-more-button" class="button is-large">Learn more</a>
+              </div>
+            </div>
+          </section>
+        </div><!-- .site-branding -->
+      </header><!-- #masthead -->
---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * acton Theme Customizer
+ *
+ * @package acton
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function acton_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'acton_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'acton_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'acton_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function acton_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function acton_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function acton_customize_preview_js() {
+	wp_enqueue_script( 'acton-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'acton_customize_preview_js' );
