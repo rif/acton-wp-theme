3c8a8c65ff28fafc3d52f16cec9e9b187d1ec24a
Radu Ioan Fericean <radu@fericean.ro>
2021-04-27 19:12:44 +0300

    posts page

--- page.php
+++ page.php
@@ -14,7 +14,9 @@
 
 get_header();
 ?>
-
+<?php if ( is_home() ) { ?>
+  <div id="posts">
+  <?php endif; ?>
 	<main id="primary" class="site-main">
 
 		<?php
@@ -30,7 +32,9 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
-
+<?php if ( is_home() ) { ?>
+  <div> <!-- #posts -->
+  <?php endif; ?>
 	</main><!-- #main -->
 
 <?php
